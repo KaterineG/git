1923602706ec4972ad2ae1dce8e1dc43514167f3
KaterineG <eagimadieva@gmail.com>
2021-03-07 18:16:12 +0400

    last save

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 const swiper = new Swiper('.swiper-container', {
   // Optional parameters
-  
+
   loop: true,
 
   // If we need pagination
@@ -23,3 +23,22 @@ const swiper = new Swiper('.swiper-container', {
     disableOnInteraction: false,
   },
 });
+/*Выпадающий список*/
+document.addEventListener('DOMContentLoaded', function () {
+
+  document.querySelectorAll('.select').forEach(function (selectBtn) {
+    selectBtn.addEventListener('click', function (event) {
+      const path = event.currentTarget.dataset.item;
+      const currOption = document.querySelector(`[data-dropdown="${path}"]`);//текущий выбор
+      if (currOption.classList.contains('active')) {
+        currOption.classList.remove('active');
+      }//если установлен актив, то просто снимаем
+      else {
+        document.querySelectorAll('.option').forEach(function (dropdown) {
+          dropdown.classList.remove('active');//снимаем актив у всех
+          currOption.classList.add('active');//добавляем актив к текущему выбору
+        })
+      }
+    })
+  })
+})
